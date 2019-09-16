46f19b5b2b79a7ce70427b3b0063782441f2763b
HaeJi-Woo <whj1939@cu.ac.kr>
2019-09-16 13:53:42 +0900

    add

--- ch01/1-8.cpp
+++ ch01/1-8.cpp
@@ -3,17 +3,13 @@
 
 int main(void)
 {
-	int i, j;
-	int n;
-
-	printf("사각형을 출력합니다.\n");
+	int i, j, n;
 	do {
-		printf("입력할 수 : ");
+		printf("몇 단 삼각형입니까? : ");
 		scanf("%d", &n);
 	} while (n <= 0);
-
 	for (i = 1; i <= n; i++) {
-		for (j = 1; j <= n; j++)
+		for (j = 1; j <= i; j++)
 			putchar('*');
 		putchar('\n');
 	}
